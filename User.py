1cc815415907f6322bdef4c50c360c12fff5ab54
viewtifultiger <viewtifultiger@gmail.com>
2025-03-11 21:32:22 -0700

    feat: User balance socials

--- User.py
+++ User.py
@@ -2,6 +2,37 @@ from login_manager import login_user
 
 class User:
 	def __init__(self, USERNAME: str , PASSWORD: str):
+		# CLIENT
 		self.cl = login_user(USERNAME, PASSWORD)
 		self.cl.delay_range = [29, 50]
-		self.id = self.cl.user_id
+
+
+		self.user_id = self.cl.user_id
+
+	def balance_socials(self):
+		followers = self.cl.user_followers(personal_id)
+		following = self.cl.user_following(personal_id)
+
+		followers_set = set(followers.items())
+		following_set = set(following.items())
+
+		# FOLLOWING (find the one I need to follow)
+		# Check to see if to_follow list has any private accounts
+		# to_follow = dict(followers_set - following_set)
+		# for user_id in to_follow:
+		# 	if cl.user_info(user_id).dict()['is_private']:
+		# 		# Keep track of time I requested to follow someone if they were private
+		# 		# this needs a database
+		# 	# Follow everyone in the to_follow list
+		# 	username = to_follow[user_id].dict()['username']
+		# 	print(f"Following: {username}")
+		# 	cl.user_follow(user_id)
+
+		to_follow = dict(followers_set - following_set)
+		for user_id in to_follow:
+			if not cl.user_info(user_id).dict()['is_private']:
+				username = to_follow[user_id].dict()['username']
+				print(f"Following: {username}")
+				cl.user_follow(user_id)
+
+		# differences = dict(followers_set ^ following_set)
--- main.py
+++ main.py
@@ -10,16 +10,7 @@ USERNAME, PASSWORD = config.USERNAME, config.PASSWORD
 
 personal = User(USERNAME, PASSWORD)
 
-# followers = cl.user_followers(personal_id)
-# following = cl.user_following(personal_id)
-
-# followers_set = set(followers.items())
-# following_set = set(following.items())
-
-# differences = dict(followers_set ^ following_set)
 # for account in differences:
-
-
 
 # # Check to see how long I've last followed everyone in private list
 # # this needs a database
@@ -27,17 +18,7 @@ personal = User(USERNAME, PASSWORD)
 # # user_id (from database)
 # cl.user_unfollow(user_id)
 
-# # FOLLOWING (find the one I need to follow)`
-# # Check to see if to_follow list has any private accounts
-# to_follow = dict(followers_set - following_set)
-# for user_id in to_follow:
-# 	if cl.user_info(user_id).dict()['is_private']:
-# 		# Keep track of time I requested to follow someone if they were private
-# 		# this needs a database
-# 	# Follow everyone in the to_follow list
-# 	username = to_follow[user_id].dict()['username']
-# 	print(f"Following: {username}")
-# 	cl.user_follow(user_id)
+
 
 # # UNFOLLOWING (find the ones I need to unfollow)
 # to_unfollow = dict(following_set - followers_set)
